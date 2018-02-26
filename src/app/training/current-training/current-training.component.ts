68d0359c8d72e1a5cec0c3907f694816c1cb9f12
FUSION\nikola.trajkovic <nikola.trajkovic@bluecouchdigital.com>
2018-02-26 12:00:10 +0100

    Passing Data to the Dialog and added "Exit" and "Continue" options

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { StopTrainingComponent } from '../stop-training/stop-training.component';
 
@@ -8,13 +8,18 @@ import { StopTrainingComponent } from '../stop-training/stop-training.component'
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() traningExit = new EventEmitter<void>();
   progress = 0;
   timer: number;
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
-   this.timer = setInterval(() => {
+    this.startOrResumeTimer();
+  }
+
+  startOrResumeTimer() {
+    this.timer = setInterval(() => {
       this.progress = this.progress + 20;
       if (this.progress >= 100) {
         clearInterval(this.timer);
@@ -24,7 +29,18 @@ export class CurrentTrainingComponent implements OnInit {
 
   onStop() {
     clearInterval(this.timer);
-    this.dialog.open(StopTrainingComponent);
-  }
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress
+      }
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.traningExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }
+    });
+  }
 }
