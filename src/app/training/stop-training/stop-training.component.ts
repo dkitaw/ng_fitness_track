87ebd6c0a044ae9f9c5436494d8d681e6f3d3f18
FUSION\nikola.trajkovic <nikola.trajkovic@bluecouchdigital.com>
2018-02-28 15:07:27 +0100

    Hendling Complete and Cancel events

--- src/app/training/stop-training/stop-training.component.ts
+++ src/app/training/stop-training/stop-training.component.ts
@@ -8,7 +8,7 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 })
 export class StopTrainingComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) { }     // Give it to me data from dialog and pass to private passedData prop
+  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) { }     // Give it data from dialog and pass to private passedData prop
 
   ngOnInit() {
   }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from "@angular/core";
-import { Exercise } from "./exercise.model";
+import { Exercise } from './exercise.model';
 import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class TrainingService {
   exerciseChanged = new Subject<Exercise>();
-  
+  private runningExercise: Exercise;
+  private exercises: Exercise[] = [];
+
   private availableExdercises: Exercise[] = [
     { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
     { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
@@ -13,8 +15,6 @@ export class TrainingService {
     { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
   ];
 
-  private runningExercise: Exercise;
-    
   getAvailableExercises() {
     return this.availableExdercises.slice();
   }
@@ -24,5 +24,31 @@ export class TrainingService {
       ex => ex.id === selectedId
     );
     this.exerciseChanged.next({...this.runningExercise});
+  }
+
+  completeExercise() {
+    this.exercises.push({ 
+      ...this.runningExercise, 
+      date: new Date(), 
+      state: 'completed'
+    });                                      // Push runningExercise to array before we set them to null and remove them
+    this.runningExercise = null;           
+    this.exerciseChanged.next(null);         // This will mean we got no running exercise
+  }
+
+  cancelExercise(progress: number) {
+    this.exercises.push({ 
+      ...this.runningExercise, 
+      duration: this.runningExercise.duration * (progress / 100),
+      calories: this.runningExercise.duration * (progress / 100),
+      date: new Date(), 
+      state: 'cancelled'
+    });                  
+    this.runningExercise = null;           
+    this.exerciseChanged.next(null); 
+  }
+
+  getRunningExercise() {
+    return { ...this.runningExercise };
   }
 }
