bc46d0d08c2c23a5117e40e57400ca5ad5664ec1
FUSION\nikola.trajkovic <nikola.trajkovic@bluecouchdigital.com>
2018-03-04 10:32:20 +0100

    Listening to snapshotChanges of firestore

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,22 +1,36 @@
 import { Injectable } from "@angular/core";
 import { Exercise } from './exercise.model';
 import { Subject } from 'rxjs/Subject';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Injectable()
 export class TrainingService {
-  
   exerciseChanged = new Subject<Exercise>();
-  private availableExdercises: Exercise[] = [
-    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
-    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
-    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
-  ];
+  exercisesChanged = new Subject<Exercise[]>();
+  private availableExdercises: Exercise[] = [];
   private runningExercise: Exercise;
   private exercises: Exercise[] = [];
 
-  getAvailableExercises() {
-    return this.availableExdercises.slice();
+  constructor(private db: AngularFirestore) { }
+
+  fetchAvailableExercises() {
+    this.db
+      .collection('availableExercises')
+      .snapshotChanges()                       // event lister which contain some metadata
+      .map(docArray => {
+        return docArray.map(doc => {
+          return {
+            id: doc.payload.doc.id,
+            name: doc.payload.doc.data().name,
+            duration: doc.payload.doc.data().duration,
+            calories: doc.payload.doc.data().calories
+          };
+        })
+      })
+      .subscribe((exercises: Exercise[]) => {
+        this.availableExdercises = exercises;
+        this.exercisesChanged.next([...this.availableExdercises]);
+      });        
   }
 
   startExercise(selectedId: string) {
